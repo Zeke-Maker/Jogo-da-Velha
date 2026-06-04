76d97ad35832d07b94a78e4f2c725712467a0082
Arthur <arthursecundario2011@gmail.com>
2026-06-04 10:36:56 -0300

    Implementação de todas as condições de vitória

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import { useState } from 'react';  
+import { useState } from "react";
 
-function Square({valor, func}) {
-  return <button className="square" onClick={func}>{valor}</button>
+function Square({ valor, func }) {
+  return (
+    <button className="square" onClick={func}>
+      {valor}
+    </button>
+  );
 }
 
 export default function Campo() {
@@ -9,12 +13,11 @@ export default function Campo() {
   const [estado, setEstado] = useState(false);
   const [status, setStatus] = useState(null);
 
-
   function reiniciarJogo() {
-    setQuadrados (Array(9).fill(null));
+    setQuadrados(Array(9).fill(null));
     setEstado(false);
     setStatus(null);
-}
+  }
 
   function mostrarJogador() {
     if (estado == false) {
@@ -24,30 +27,50 @@ export default function Campo() {
     }
   }
 
-
   function calcularVencedor() {
-    if ((quadrados[0]=="X" && quadrados[1]=="X" && quadrados[2]=="X")
-      || (quadrados[3]=="X" && quadrados[4]=="X" && quadrados[5]=="X")
-      || (quadrados[6]=="X" && quadrados[7]=="X" && quadrados[8]=="X")
+    /* Jogador 1 */
+    if (
+      /*Linha*/
+      (quadrados[0] == "X" && quadrados[1] == "X" && quadrados[2] == "X") ||
+      (quadrados[3] == "X" && quadrados[4] == "X" && quadrados[5] == "X") ||
+      (quadrados[6] == "X" && quadrados[7] == "X" && quadrados[8] == "X") ||
+      /*Coluna*/
+      (quadrados[0] == "X" && quadrados[3] == "X" && quadrados[6] == "X") ||
+      (quadrados[1] == "X" && quadrados[4] == "X" && quadrados[7] == "X") ||
+      (quadrados[2] == "X" && quadrados[5] == "X" && quadrados[8] == "X") ||
+      /*Diagonal*/
+      (quadrados[0] == "X" && quadrados[4] == "X" && quadrados[8] == "X") ||
+      (quadrados[2] == "X" && quadrados[4] == "X" && quadrados[6] == "X")
     ) {
       console.log("Jogador 1 Venceu!");
       return "Jogador 1 venceu!";
-    } else if ((quadrados[0]=="O" && quadrados[1]=="O" && quadrados[2]=="O")
-      || (quadrados[3]=="O" && quadrados[4]=="O" && quadrados[5]=="O")
-      || (quadrados[6]=="O" && quadrados[7]=="O" && quadrados[8]=="O")
+    } 
+    /* Jogador 2 */
+    else if (
+      /*Linha*/
+      (quadrados[0] == "O" && quadrados[1] == "O" && quadrados[2] == "O") ||
+      (quadrados[3] == "O" && quadrados[4] == "O" && quadrados[5] == "O") ||
+      (quadrados[6] == "O" && quadrados[7] == "O" && quadrados[8] == "O") ||
+      /*Coluna*/
+      (quadrados[0] == "O" && quadrados[3] == "O" && quadrados[6] == "O") ||
+      (quadrados[1] == "O" && quadrados[4] == "O" && quadrados[7] == "O") ||
+      (quadrados[2] == "O" && quadrados[5] == "O" && quadrados[8] == "O") ||
+      /*Diagonal*/    
+      (quadrados[0] == "O" && quadrados[4] == "O" && quadrados[8] == "O") ||
+      (quadrados[2] == "O" && quadrados[4] == "O" && quadrados[6] == "O") 
     ) {
-        console.log("Jogador 2 Venceu!");
-        return "Jogador 2 venceu!";
+      console.log("Jogador 2 Venceu!");
+      return "Jogador 2 venceu!";
     } else if (
-      quadrados[0]!=null &&
-      quadrados[1]!=null &&
-      quadrados[2]!=null &&
-      quadrados[3]!=null &&
-      quadrados[4]!=null &&
-      quadrados[5]!=null &&
-      quadrados[6]!=null &&
-quadrados[7]!=null &&
-quadrados[8]!=null
+      quadrados[0] != null &&
+      quadrados[1] != null &&
+      quadrados[2] != null &&
+      quadrados[3] != null &&
+      quadrados[4] != null &&
+      quadrados[5] != null &&
+      quadrados[6] != null &&
+      quadrados[7] != null &&
+      quadrados[8] != null
     ) {
       console.log("Deu empate!");
       return "Deu empate!";
@@ -57,11 +80,11 @@ quadrados[8]!=null
 
   function handleClick(i) {
     const quadradoTemp = quadrados.slice();
-    if (quadradoTemp[i]!=null) {
+    if (quadradoTemp[i] != null) {
       return;
     }
 
-    if (estado==false) {
+    if (estado == false) {
       quadradoTemp[i] = "X";
     } else {
       quadradoTemp[i] = "O";
@@ -71,26 +94,32 @@ quadrados[8]!=null
 
     setStatus(calcularVencedor());
   }
-  return <>
-    <div class="board-row">
-      <Square valor={quadrados[0]} func={() => handleClick(0)} />
-      <Square valor={quadrados[1]} func={() =>handleClick(1)}/>
-      <Square valor={quadrados[2]} func={() =>handleClick(2)}/>
-    </div>
-    <div class="board-row">
-      <Square valor={quadrados[3]} func={() =>handleClick(3)}/>
-      <Square valor={quadrados[4]} func={() =>handleClick(4)}/>
-      <Square valor={quadrados[5]} func={() =>handleClick(5)}/>
-    </div>
-    <div class="board-row">
-      <Square valor={quadrados[6]} func={() =>handleClick(6)}/>
-      <Square valor={quadrados[7]} func={() =>handleClick(7)}/>
-      <Square valor={quadrados[8]} func={() =>handleClick(8)}/>  
-    </div>
-    <div><h2>{mostrarJogador()}</h2></div>
-    <div><h1>{status}</h1></div>
-    <div><button onClick={reiniciarJogo}>Reiniciar</button></div>
-
-
-  </>;
+  return (
+    <>
+      <div class="board-row">
+        <Square valor={quadrados[0]} func={() => handleClick(0)} />
+        <Square valor={quadrados[1]} func={() => handleClick(1)} />
+        <Square valor={quadrados[2]} func={() => handleClick(2)} />
+      </div>
+      <div class="board-row">
+        <Square valor={quadrados[3]} func={() => handleClick(3)} />
+        <Square valor={quadrados[4]} func={() => handleClick(4)} />
+        <Square valor={quadrados[5]} func={() => handleClick(5)} />
+      </div>
+      <div class="board-row">
+        <Square valor={quadrados[6]} func={() => handleClick(6)} />
+        <Square valor={quadrados[7]} func={() => handleClick(7)} />
+        <Square valor={quadrados[8]} func={() => handleClick(8)} />
+      </div>
+      <div>
+        <h2>{mostrarJogador()}</h2>
+      </div>
+      <div>
+        <h1>{status}</h1>
+      </div>
+      <div>
+        <button onClick={reiniciarJogo}>Reiniciar</button>
+      </div>
+    </>
+  );
 }
